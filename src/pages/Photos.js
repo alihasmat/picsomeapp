a70e8fef1114baaa94a02b153ecc25601583b72c
Mac <mac@Macs-MacBook-Air.local>
2020-06-20 09:54:04 +0300

    setup react router, click on pic some logo and go to photos component, click on cart logo and go to cart components

---
+++ src/pages/Photos.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Photos() {
+    return(
+        <main className="photos">
+            <h1>Images goes here</h1>
+        </main>
+    )
+}
+
+export default Photos
