1e24fefcc57f607918bc7c270b97b929bca1c79e
Mac <mac@Macs-MacBook-Air.local>
2020-06-22 11:52:09 +0300

    used map function

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
+import {appContext} from "../appContext"
+import Image from "../components/image"
+import {ImgClass} from "../utils/imgClass"
 
 function Photos() {
+    const {images} = useContext(appContext)
+     //console.log(images)
+
+     {/*const photosElement = images.map(img => (
+         <div className="image-container">
+             <img key={img.id} src={img.url} className="image-grid" />
+         </div>
+     )) */}
+
+
+    const photosElement = images.map((img, i) => (
+        <Image key={img.id} img={img} className={ImgClass(i)} />
+    ))
+
     return(
         <main className="photos">
-            <h1>Images goes here</h1>
+            {photosElement}
         </main>
     )
 }
