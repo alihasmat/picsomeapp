af72e1853992dc476761dd9f9848120b7d4c0329
Mac <mac@Macs-MacBook-Air.local>
2020-06-23 14:52:22 +0300

    worked on toggleMethod

--- src/components/image.js
+++ src/components/image.js
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import {appContext} from "../appContext"
 
 function Image(props) {
+    const [hovered, setHovered] = useState(false)
+    const {toggleMethod} = useContext(appContext)
+
+    function heartIcon() {
+        if(props.img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleMethod(props.img.id)}></i>
+        }else if(hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleMethod(props.img.id)}></i>
+        }
+    }
+    //const heartIcon = hovered && (<i className="ri-heart-line favorite" onClick={() => toggleMethod(props.img.id)}></i>)
+    const cartIcon = hovered && (<i className="ri-add-circle-line cart"></i>)
+
     return (
-        <div className={`${props.className} image-container`}>
+        <div 
+        className={`${props.className} image-container`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        >
             <img src={props.img.url} className="image-grid" />
+            {heartIcon()}
+            {cartIcon}
+
         </div>
     )
 }
